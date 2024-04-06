cf8e35b90ed9b3ebfc72759f619570a14d60fb45
suraj_singh <skcse47@gmail.com>
2024-04-06 23:17:29 +0530

    sidebar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 
 function App() {
 
   return (
     <>
       <div className='p-4 h-screen flex items-center justify-center'>
-        <Login />
+        <Home />
       </div>
     </>
   )
