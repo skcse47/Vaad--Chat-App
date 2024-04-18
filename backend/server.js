4b713848426b06ff4870634f463c0033a5bbccca
suraj_singh <skcse47@gmail.com>
2024-04-19 00:23:56 +0530

    socket io connection

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,10 @@ import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import getAllUsers from "./routes/user.routes.js"
 import connectToMongoDb from "./db/connectToMongoDb.js";
+import { server, app } from "./socket/socket.js";
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 5000;
-    
 dotenv.config();
 
 app.use(express.json());
@@ -25,7 +25,7 @@ app.use("/api/message", messageRoutes);
 app.use("/api/user", getAllUsers);
 
 
-app.listen(5000, () => {
+server.listen(5000, () => {
     connectToMongoDb();
     console.log(`Server is running on ${PORT}`)
 })
---
+++ backend/socket/socket.js
@@ -0,0 +1,40 @@
+import {Server} from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+// console.log("Www");return
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST"],
+    }
+});
+
+// {"userId": "socket.id"}
+var userSocketMap = {};
+
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+
+    const userId = socket.handshake.query.userId;
+
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+
+    io.emit("onlineUsers", Object.keys(userSocketMap));
+    // socket.on() is used to listen to the events. Can be used both at client and server end.
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("onlineUsers", Object.keys(userSocketMap));
+    });
+});
+
+
+
+
+export {app, server, io}
