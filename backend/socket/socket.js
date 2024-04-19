285c1522b9ac393c730996ff61ae77cbf7e47cc6
suraj_singh <skcse47@gmail.com>
2024-04-20 00:55:21 +0530

    deploy

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 // console.log("Www");return
 const io = new Server(server, {
     cors: {
-        origin: ["http://vaad-chat-app.netlify.app"],
+        origin: ["http://localhost:5173"],
         methods: ["GET", "POST"],
     }
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     // port: 3000,
     proxy: {
       "/api": {
-        target: "https://vaad-chat-app.onrender.com",
+        target: "http://localhost:5000",
         // rewrite: (path) => path.replace(/^\/api/, ''), // Remove the "/api" prefix
       }
     }
