596df62219d5dc016b7b9ce897288ba3269e168f
suraj_singh <skcse47@gmail.com>
2024-04-19 18:30:05 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,26 @@ import messageRoutes from "./routes/message.routes.js"
 import getAllUsers from "./routes/user.routes.js"
 import connectToMongoDb from "./db/connectToMongoDb.js";
 import { server, app } from "./socket/socket.js";
+import path from "path";
 
 // const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req,res) => {
-    res.send("Home");
-})
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/user", getAllUsers);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 
 server.listen(5000, () => {
